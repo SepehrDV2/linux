e9b5ae258294d867c124aebd176c9dbd39d408a7
wei zhang <wei.zhang.gbs@gmail.com>
2021-03-06 04:58:18 +0000

    fix errors

--- include/linux/userfaultfd_k.h
+++ include/linux/userfaultfd_k.h
@@ -77,7 +77,7 @@ extern long uffd_wp_range(struct mm_struct *dst_mm, struct vm_area_struct *vma,
 			  unsigned long start, unsigned long len, bool enable_wp);
 			       bool enable_wp, bool *mmap_changing);
 extern ssize_t dma_mcopy_pages(struct mm_struct *dst_mm,
-				struct userfaultfd_dma_copy *userfaultfd_dma_copy,
+				struct uffdio_dma_copy *uufdio_dma_copy,
 			    	bool *mmap_changing);
 
 /* mm helpers */
